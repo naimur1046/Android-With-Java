e7a027e5b24aeeebaed6beb026c09c9801dee93d
naimur rahman rumel <naimurrahman1046@gmail.com>
2024-06-26 23:23:02 +0600

    In this section I have learn the Intent

---
+++ app/src/main/java/com/example/intentunderstand/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.intentunderstand;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button clickToNext;
+    private Intent nextActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        clickToNext=findViewById(R.id.nextActivity);
+        clickToNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextActivity= new Intent(getApplicationContext(),SecondActivity.class);
+                startActivity(nextActivity);
+
+            }
+        });
+    }
+}
