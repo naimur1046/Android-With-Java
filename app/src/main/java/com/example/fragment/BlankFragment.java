6745f7fe2b0d508e7ab4fb186377971d410cd3e0
naimur rahman rumel <naimurrahman1046@gmail.com>
2024-07-04 18:11:48 +0600

    I have learnt the Static Fragment here

---
+++ app/src/main/java/com/example/fragment/BlankFragment.java
@@ -0,0 +1,28 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class BlankFragment extends Fragment {
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        TextView textView = view.findViewById(R.id.txtFragment);
+
+        return  view;
+    }
+}
